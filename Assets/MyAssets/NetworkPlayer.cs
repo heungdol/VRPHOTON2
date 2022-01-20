4af6b4366e0d93a24a4644b1af115aac1f488442
이다흥 <30585313+heungdol@users.noreply.github.com>
2022-01-20 12:16:56 +0900

    플레이어 ㅜㅅ정

--- Assets/MyAssets/NetworkPlayer.cs
+++ Assets/MyAssets/NetworkPlayer.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 using Photon.Pun;
+using Unity.XR.CoreUtils;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class NetworkPlayer : MonoBehaviour
 {
@@ -10,11 +12,20 @@ public class NetworkPlayer : MonoBehaviour
     public Transform leftHand;
     public Transform rightHand;
     private PhotonView photonView;
+
+    private Transform headRig;
+    private Transform leftHandRig;
+    private Transform rightHandRig;
     
     // Start is called before the first frame update
     void Start()
     {
         photonView = GetComponent<PhotonView>();
+
+        XROrigin rig = FindObjectOfType<XROrigin>();
+        headRig = rig.transform.Find("Camera Offset/Main Camera");
+        leftHandRig = rig.transform.Find("Camera Offset/LeftHand Controller");
+        rightHandRig = rig.transform.Find("Camera Offset/RightHand Controller");
     }
 
     // Update is called once per frame
@@ -26,18 +37,24 @@ public class NetworkPlayer : MonoBehaviour
             leftHand.gameObject.SetActive(false);
             rightHand.gameObject.SetActive(false);
             
-            MapPosition(head, XRNode.Head);
-            MapPosition(leftHand, XRNode.LeftHand);
-            MapPosition(rightHand, XRNode.RightHand);
+            MapPosition(head, headRig);
+            MapPosition(leftHand, leftHandRig);
+            MapPosition(rightHand, rightHandRig);
         }
     }
 
-    void MapPosition(Transform target, XRNode node)
+    /*void MapPosition(Transform target, XRNode node)
     {
         InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
         InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);
 
         target.position = position;
         target.rotation = rotation;
+    }*/
+
+    void MapPosition(Transform target, Transform rigTransform)
+    {
+        target.position = rigTransform.position;
+        target.rotation = rigTransform.rotation;
     }
 }
