39ff95516416b91386080e87f72614e3273e0b55
이다흥 <30585313+heungdol@users.noreply.github.com>
2022-01-20 12:33:58 +0900

    손가락 포톤뷰 추가

--- Assets/MyAssets/NetworkPlayer.cs
+++ Assets/MyAssets/NetworkPlayer.cs
@@ -11,6 +11,10 @@ public class NetworkPlayer : MonoBehaviour
     public Transform head;
     public Transform leftHand;
     public Transform rightHand;
+
+    public Animator leftHandAnimator;
+    public Animator rightHandAnimator;
+    
     private PhotonView photonView;
 
     private Transform headRig;
@@ -40,6 +44,9 @@ public class NetworkPlayer : MonoBehaviour
             MapPosition(head, headRig);
             MapPosition(leftHand, leftHandRig);
             MapPosition(rightHand, rightHandRig);
+
+            UpdateHandAnimation(InputDevices.GetDeviceAtXRNode(XRNode.LeftHand), leftHandAnimator);
+            UpdateHandAnimation(InputDevices.GetDeviceAtXRNode(XRNode.RightHand), rightHandAnimator);
         }
     }
 
@@ -56,5 +63,26 @@ public class NetworkPlayer : MonoBehaviour
     {
         target.position = rigTransform.position;
         target.rotation = rigTransform.rotation;
+    }
+    
+    void UpdateHandAnimation(InputDevice targetDevice, Animator handAnimator)
+    {
+        if(targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Trigger", 0);
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Grip", 0);
+        }
     }
 }
